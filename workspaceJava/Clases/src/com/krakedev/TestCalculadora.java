76adddd33656897e9f9a17600d0efe9f061a2f0b
Erika-Andrade <erikaandradesuz@gmail.com>
2024-09-27 13:26:34 -0500

    funcion dividir testeada

--- workspaceJava/Clases/src/com/krakedev/TestCalculadora.java
+++ workspaceJava/Clases/src/com/krakedev/TestCalculadora.java
@@ -7,16 +7,19 @@ public class TestCalculadora {
 		int resultadoSuma;
 		int resultadoResta;
 		double resultadoMulti;
+		double resultadoDivi;
 		
 		calcu=new Calculadora();
 		
 		resultadoSuma=calcu.sumar(2,3);
 		resultadoResta=calcu.restar(10,3);
 		resultadoMulti=calcu.multiplicar(10,5);
+		resultadoDivi=calcu.dividir(10,5);
 		
 		System.out.println("RESULTADO: "+resultadoSuma);
 		System.out.println("RESULTADO RESTA: "+resultadoResta);
 		System.out.println("RESULTADO MULTIPLICACION: "+resultadoMulti);
+		System.out.println("RESULTADO DIVISION: "+resultadoDivi);
 		
 	}
 
