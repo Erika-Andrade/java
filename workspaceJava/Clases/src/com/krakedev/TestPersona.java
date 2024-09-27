6bce3e154579515693a2da35f46e142b030017b9
Erika-Andrade <erikaandradesuz@gmail.com>
2024-09-27 10:08:13 -0500

    Agregar carpeta workspace y proyecto clases

---
+++ workspaceJava/Clases/src/com/krakedev/TestPersona.java
@@ -0,0 +1,17 @@
+package com.krakedev;
+
+public class TestPersona {
+
+	public static void main(String[] args) {
+		Persona p;//1.-Declaro una variable called p tipo Persona
+		Persona p2=new Persona();
+		p=new Persona();
+		p.nombre="Holanda";
+		System.out.println("nombre: "+p.nombre);
+		System.out.println("edad: "+p.edad);
+		System.out.println("estatura: "+p.estatura);
+		
+
+	}
+
+}
