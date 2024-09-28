432b47c95220c01c53e17cadea2bc0c3c9ab7e85
Erika-Andrade <erikaandradesuz@gmail.com>
2024-09-27 23:55:44 -0500

    Encapsular producto y test producto

--- workspaceJava/Clases/src/com/krakedev/TestProducto.java
+++ workspaceJava/Clases/src/com/krakedev/TestProducto.java
@@ -7,38 +7,38 @@ public class TestProducto {
 		Producto productoB=new Producto();
 		Producto productoC=new Producto();
 		//---modificar valores atributos producto a
-		productoA.nombre="Acetaminofén (Tylenol)";
-		productoA.descripcion="Analgésico y antipirético";
-		productoA.precio=5.99;
-		productoA.stockActual=25;
+		productoA.setNombre("Acetaminofén (Tylenol)");
+		productoA.setDescripcion("Analgésico y antipirético");
+		productoA.setPrecio(5.99);
+		productoA.setStockActual(25);
 		//---modificar valores atributos producto b
-		productoB.nombre="Ibuprofeno (Advil)";
-		productoB.descripcion="Antiinflamatorio y analgésico";
-		productoB.precio=8.99;
-		productoB.stockActual=40;
+		productoB.setNombre("Ibuprofeno (Advil)");
+		productoB.setDescripcion("Antiinflamatorio y analgésico");
+		productoB.setPrecio(8.99);
+		productoB.setStockActual(40);
 				//---modificar valores atributos producto c
-		productoC.nombre="Cremas para manos Nive";
-		productoC.descripcion="Hidratación intensiva para manos secas";
-		productoC.precio=7.49;
-		productoC.stockActual=12;
+		productoC.setNombre("Cremas para manos Nivea");
+		productoC.setDescripcion("Hidratación intensiva para manos secas");
+		productoC.setPrecio(7.49);
+		productoC.setStockActual(12);
 		//mostrar producto A
 		System.out.println("-------Producto A------");
-		System.out.println("nombre: "+productoA.nombre);
-		System.out.println("descripción: "+productoA.descripcion);
-		System.out.println("precio: "+productoA.precio);
-		System.out.println("Stock Actual: "+productoA.stockActual);
+		System.out.println("nombre: "+productoA.getNombre());
+		System.out.println("descripción: "+productoA.getDescripcion());
+		System.out.println("precio: "+productoA.getPrecio());
+		System.out.println("Stock Actual: "+productoA.getStockActual());
 		//mostrar producto B
 		System.out.println("-------Producto B------");
-		System.out.println("nombre: "+productoB.nombre);
-		System.out.println("descripción: "+productoB.descripcion);
-		System.out.println("precio: "+productoB.precio);
-		System.out.println("Stock Actual: "+productoB.stockActual);
+		System.out.println("nombre: "+productoB.getNombre());
+		System.out.println("descripción: "+productoB.getDescripcion());
+		System.out.println("precio: "+productoB.getPrecio());
+		System.out.println("Stock Actual: "+productoB.getStockActual());
 		//mostrar producto C
 		System.out.println("-------Producto C------");
-		System.out.println("nombre: "+productoC.nombre);
-		System.out.println("descripción: "+productoC.descripcion);
-		System.out.println("precio: "+productoC.precio);
-		System.out.println("Stock Actual: "+productoC.stockActual);
+		System.out.println("nombre: "+productoC.getNombre());
+		System.out.println("descripción: "+productoC.getDescripcion());
+		System.out.println("precio: "+productoC.getPrecio());
+		System.out.println("Stock Actual: "+productoC.getStockActual());
 		
 
 	}
