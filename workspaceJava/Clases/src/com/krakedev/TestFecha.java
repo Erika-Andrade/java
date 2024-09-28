82d3e5cb78e74d53c6627b973aeaab4d47f545a3
Erika-Andrade <erikaandradesuz@gmail.com>
2024-09-28 13:50:57 -0500

    constrcutos con parametros en fecha

--- workspaceJava/Clases/src/com/krakedev/TestFecha.java
+++ workspaceJava/Clases/src/com/krakedev/TestFecha.java
@@ -3,14 +3,19 @@ package com.krakedev;
 public class TestFecha {
 
 	public static void main(String[] args) {
-		Fecha fecha1=new Fecha();
-		fecha1.setAnio(2000);
+		Fecha fecha1=new Fecha(2001,4,3);
+		Fecha fecha2=new Fecha(2000,3,12);
+		/*cha1.setAnio(2000);
 		fecha1.setMes(12);
-		fecha1.setDia(12);
+		fecha1.setDia(12);*/
 		
 		System.out.println(fecha1.getAnio());
 		System.out.println(fecha1.getMes());
 		System.out.println(fecha1.getDia());
+		
+		System.out.println(fecha2.getAnio());
+		System.out.println(fecha2.getMes());
+		System.out.println(fecha2.getDia());
 
 	}
 
