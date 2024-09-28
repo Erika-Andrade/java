0c3fb935953cb6b68f1e37c7656645c78b1fdb35
Erika-Andrade <erikaandradesuz@gmail.com>
2024-09-27 23:48:22 -0500

    encapsular persona y test

--- workspaceJava/Clases/src/com/krakedev/TestPersona.java
+++ workspaceJava/Clases/src/com/krakedev/TestPersona.java
@@ -4,12 +4,12 @@ public class TestPersona {
 
 	public static void main(String[] args) {
 		Persona p;//1.-Declaro una variable called p tipo Persona
-		Persona p2=new Persona();
+		//Persona p2=new Persona();
 		p=new Persona();
-		p.nombre="Holanda";
-		System.out.println("nombre: "+p.nombre);
-		System.out.println("edad: "+p.edad);
-		System.out.println("estatura: "+p.estatura);
+		p.setNombre("Holanda");
+		System.out.println("nombre: "+p.getNombre());
+		System.out.println("edad: "+p.getEdad());
+		System.out.println("estatura: "+p.getEstatura());
 		
 
 	}
