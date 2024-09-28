d8b2035052a4e9cdd6cc75ad880831005930f912
Erika-Andrade <erikaandradesuz@gmail.com>
2024-09-27 23:58:31 -0500

    Encapsular Rectangulo y TestRectangulo

--- workspaceJava/Clases/src/com/krakedev/TestRectangulo.java
+++ workspaceJava/Clases/src/com/krakedev/TestRectangulo.java
@@ -10,11 +10,11 @@ public class TestRectangulo {
 		double perimetro1;
 		double perimetro2;
 		
-		r1.base=10;
-		r1.altura=5;
+		r1.setBase(10);;
+		r1.setAltura(5);;
 		
-		r2.base=8;
-		r2.altura=3;
+		r2.setBase(8);;
+		r2.setAltura(3);;
 		
 		area=r1.calcularArea();
 		perimetro1=r1.calcularPerimetro();
