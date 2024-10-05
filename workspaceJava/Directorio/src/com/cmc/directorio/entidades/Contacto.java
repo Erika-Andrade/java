f32e1a13aff6113b424ffba33531468772d19266
Erika-Andrade <erikaandradesuz@gmail.com>
2024-10-04 22:50:57 -0500

    Clase contactos

--- workspaceJava/Directorio/src/com/cmc/directorio/entidades/Contacto.java
+++ workspaceJava/Directorio/src/com/cmc/directorio/entidades/Contacto.java
@@ -13,4 +13,35 @@ public class Contacto {
 		this.telefono = telefono;
 		this.peso = peso;
 	}
+	public String getNombre() {
+		return nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public boolean isActivo() {
+		return activo;
+	}
+	public Telefono getTelefono() {
+		return telefono;
+	}
+	public double getPeso() {
+		return peso;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public void setActivo(boolean activo) {
+		this.activo = activo;
+	}
+	public void setTelefono(Telefono telefono) {
+		this.telefono = telefono;
+	}
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	
 }
---
+++ workspaceJava/Directorio/src/com/cmc/directorio/test/TestContacto1.java
@@ -0,0 +1,15 @@
+package com.cmc.directorio.test;
+
+import com.cmc.directorio.entidades.Contacto;
+import com.cmc.directorio.entidades.Telefono;
+public class TestContacto1 {
+
+	public static void main(String[] args) {
+		Telefono telf=new Telefono("claro","0978605848",10);
+		Contacto c=new Contacto("Alejandra","Pizarnik",telf,48.9);
+		System.out.println(c.getNombre()+" "+c.getApellido()+" "
+		+c.getPeso()+" "+telf.getOperadora()+" "+telf.getNombre());
+
+	}
+
+}
