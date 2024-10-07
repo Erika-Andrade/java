0f6fe9da3bf43b55bb0462dc61f9dd9c8def65d4
Erika-Andrade <erikaandradesuz@gmail.com>
2024-10-06 23:55:22 -0500

    paso 13 listo

--- workspaceJava/EvaluacionAE/src/com/clearminds/test/TestCelda.java
+++ workspaceJava/EvaluacionAE/src/com/clearminds/test/TestCelda.java
@@ -5,7 +5,7 @@ import com.clearminds.componentes.Celda;
 public class TestCelda {
 	public static void main(String[] args) {
 		Celda celda=new Celda("A1");
-		//El error se da en la línea 9, en la clase TestCelda con la variable nombre
+		//El error se da en la línea 9, en la clase TestCelda con la variable celda al tratar de llamar a Producto cuando su nombre esta vacio
 		System.out.println(celda.getProducto().getNombre());
 		
 		System.out.println("CELDA:"+celda.getProducto());
