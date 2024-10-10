d35c228efe2638150465d5bc0cb5d4293abbf8e4
Erika-Andrade <erikaandradesuz@gmail.com>
2024-10-10 17:06:51 -0500

    ejercicio2

--- workspaceJava/Evaluacion1ErikaAndrade/src/com/clearminds/componentes/celda.java
+++ workspaceJava/Evaluacion1ErikaAndrade/src/com/clearminds/componentes/celda.java
@@ -1,38 +1,40 @@
 package com.clearminds.componentes;
 
-public class celda {
-	private Producto producto;
-	private int stock;
-	private String codigo;
-	
-	//constructor
-	public celda(String codigo) {
-		this.codigo = codigo;
-	}
-	//getters and setters
-	public Producto getProducto() {
-		return producto;
-	}
-	
-	public int getStock() {
-		return stock;
-	}
-	public String getCodigo() {
-		return codigo;
-	}
-	public void setProducto(Producto producto) {
-		this.producto = producto;
-	}
-	public void setStock(int stock) {
-		this.stock = stock;
-	}
-	public void setCodigo(String codigo) {
-		this.codigo = codigo;
-	}
-	//metodos
-	public void ingresarProducto(Producto producto, int stock) {
-		this.producto=producto;
-		this.stock=stock;
-	}
-	
+
+public class Celda {
+		private Producto producto;
+		private int stock;
+		private String codigo;
+		
+		//constructor
+		public Celda(String codigo) {
+			this.codigo = codigo;
+		}
+		//getters and setters
+		public Producto getProducto() {
+			return producto;
+		}
+		
+		public int getStock() {
+			return stock;
+		}
+		public String getCodigo() {
+			return codigo;
+		}
+		public void setProducto(Producto producto) {
+			this.producto = producto;
+		}
+		public void setStock(int stock) {
+			this.stock = stock;
+		}
+		public void setCodigo(String codigo) {
+			this.codigo = codigo;
+		}
+		//metodos
+		public void ingresarProducto(Producto producto, int stock) {
+			this.producto=producto;
+			this.stock=stock;
+		}
+		
 }
+
