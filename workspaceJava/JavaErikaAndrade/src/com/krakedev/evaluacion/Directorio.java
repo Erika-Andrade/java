3cf5a22ed419127c47c59ccb132a430ceb70b651
Erika-Andrade <erikaandradesuz@gmail.com>
2024-10-11 12:54:52 -0500

    requerimiento 11 por crregir

--- workspaceJava/JavaErikaAndrade/src/com/krakedev/evaluacion/Directorio.java
+++ workspaceJava/JavaErikaAndrade/src/com/krakedev/evaluacion/Directorio.java
@@ -6,7 +6,7 @@ import java.util.Date;
 
 public class Directorio {
 	private ArrayList<Contacto> contactos=new ArrayList<Contacto>();
-	private Date fechaModificacion;
+	private Date fechaModificacion=new Date();
 	
 	public boolean agregarContacto(Contacto contacto) {
 		boolean existe=false;
@@ -20,6 +20,7 @@ public class Directorio {
 		if(existe==false) {
 			contactos.add(contacto);
 			agregaNuevo=true;
+			//agregar fecha 
 		}
 		return agregaNuevo;
 	}
@@ -32,5 +33,32 @@ public class Directorio {
 			}
 		}
 		return contactoEnc;
+	}
+	public String consultarUltimaModificacion() {
+		//Date fecha=this.fechaModificacion.getDate();
+		return " ";
+	}
+	public int contarPerdidos() {
+		int conta=0;
+		for(int i=0;i<contactos.size();i++) {
+			if(contactos.get(i).getDireccion()==null){
+				conta+=1;
+			}
+		}
+		return conta;
+	}
+	public int contarFijos() {
+		int conta=0;
+		ArrayList<Telefono> elemento;
+		for(int i=0;i<contactos.size();i++) {
+			elemento=contactos.get(i).getTelefono();
+			for(int a=0;a<elemento.size();i++) {
+				if(elemento.get(a).getTipo().equals("Convencional")
+						&&elemento.get(a).getEstado().equals("C") ) {
+					conta+=1;
+				}
+			}
+		}
+		return conta;
 	}
 }
