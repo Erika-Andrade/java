b3a6ad4774b4cb56a8861eb94567bc6f1077c970
Erika-Andrade <erikaandradesuz@gmail.com>
2024-10-11 17:23:23 -0500

    requerimiento 11 corregido

--- workspaceJava/JavaErikaAndrade/src/com/krakedev/evaluacion/Directorio.java
+++ workspaceJava/JavaErikaAndrade/src/com/krakedev/evaluacion/Directorio.java
@@ -9,7 +9,34 @@ import java.util.Date;
 public class Directorio {
 	private ArrayList<Contacto> contactos=new ArrayList<Contacto>();
 	private Date fechaModificacion;
+	private ArrayList<Contacto> correctos=new ArrayList<Contacto>();
+	private ArrayList<Contacto> incorrectos=new ArrayList<Contacto>();
 	
+	
+	public ArrayList<Contacto> getContactos() {
+		return contactos;
+	}
+	public Date getFechaModificacion() {
+		return fechaModificacion;
+	}
+	public void setContactos(ArrayList<Contacto> contactos) {
+		this.contactos = contactos;
+	}
+	public void setFechaModificacion(Date fechaModificacion) {
+		this.fechaModificacion = fechaModificacion;
+	}
+	public ArrayList<Contacto> getCorrectos() {
+		return correctos;
+	}
+	public ArrayList<Contacto> getIncorrectos() {
+		return incorrectos;
+	}
+	public void setCorrectos(ArrayList<Contacto> correctos) {
+		this.correctos = correctos;
+	}
+	public void setIncorrectos(ArrayList<Contacto> incorrectos) {
+		this.incorrectos = incorrectos;
+	}
 	public boolean agregarContacto(Contacto contacto) {
 		boolean existe=false;
 		boolean agregaNuevo=false;
@@ -54,7 +81,7 @@ public class Directorio {
 		ArrayList<Telefono> elemento;
 		for(int i=0;i<contactos.size();i++) {
 			elemento=contactos.get(i).getTelefono();
-			for(int a=0;a<elemento.size();i++) {
+			for(int a=0;a<elemento.size();a++) {
 				if(elemento.get(a).getTipo().equals("Convencional")
 						&&elemento.get(a).getEstado().equals("C") ) {
 					conta+=1;
@@ -62,5 +89,15 @@ public class Directorio {
 			}
 		}
 		return conta;
+	}
+	public void depurar() {
+		for(int i=0;i<contactos.size();i++) {
+			if(contactos.get(i).getDireccion()==null) {
+				incorrectos.add(contactos.get(i));
+			}else {
+				correctos.add(contactos.get(i));
+			}
+		}
+		contactos.clear();
 	}
 }
