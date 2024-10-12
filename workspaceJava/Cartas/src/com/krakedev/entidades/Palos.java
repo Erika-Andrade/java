a924db0f4d79d8d5f07226a991587342067ad7ea
Erika-Andrade <erikaandradesuz@gmail.com>
2024-10-12 08:54:32 -0500

    Poryecto Cartas creado, tres paquetes creados dentro y clase Palos con sus 4 atributos

---
+++ workspaceJava/Cartas/src/com/krakedev/entidades/Palos.java
@@ -0,0 +1,8 @@
+package com.krakedev.entidades;
+
+public class Palos {
+	private String corazonRojo="CR";
+	private String corazonNegro="CN";
+	private String diamante="DI";
+	private String trebol="TR";
+}
